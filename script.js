d3ed286b3cacde919e53316f2cbc1e4e3afbd713
StellaBellaO <101139955+StellaBellaO@users.noreply.github.com>
2022-03-26 10:45:09 +0000

    Added live forecast and removed the conversion of celsius to fahrenheit

--- script.js
+++ script.js
@@ -24,6 +24,49 @@ if (minutes < 10) {
 let time = document.querySelector(".time");
 
 time.innerHTML = `${day} <br> ${hour}:${minutes}`;
+////// Forecast
+function formatDay(timestamp){
+    let date = new date(timestamp * 1000);
+    let day = new date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
+function showForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+            forecastHTML + `
+            <div class="col-2">
+            <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+            <img
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            alt=""
+            width="45"
+            />
+            <div class="weather-temp">
+            <span class="temp-max"> ${Math.round(forecastDay.temp.max)}°</span>
+            <span class="temp-min"> ${Math.round(forecastDay.temp.min)}°</span>
+            </div>
+            </div>
+            `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "57a16d31a99c50513998174551722349"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=metrics`;
+    axios.get(apiUrl).then(showForecast);
+}
 
 //// City Search
 function showWeather(response) {
@@ -37,12 +80,14 @@ function showWeather(response) {
     document.querySelector(".weather").innerHTML = response.data.weather[0].main;
     celsiusTemperature = response.data.main.temp;
     
-    let iconElement = document.querySelector("#icon");
+    let iconElement = document.querySelector("#emoji");
+
     iconElement.setAttribute(
         "src",
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
       );
       iconElement.setAttribute("alt", response.data.weather[0].description);
+
   }
   
   function search(city) {
@@ -60,32 +105,6 @@ function showWeather(response) {
   let form = document.querySelector("#button-input");
   form.addEventListener("submit", submit);
   
-/// convert c to f
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
 
 
 ///
